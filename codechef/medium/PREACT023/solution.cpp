58dfe4b91df1444d5057179b00dfaa2e96e6f1f6
Pallabi Pandi <pallabitheprincess74@gmail.com>
2026-07-30 15:22:41 +0530

    ✅ PREACT023 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-30

---
+++ codechef/medium/PREACT023/solution.cpp
@@ -0,0 +1,25 @@
+    <div>
+      <DiscountMessage isPremiumMember={isPremiumMember} />
+    </div>
+  );
+  return (
+
+  const isPremiumMember = true; 
+function App() {
+
+};
+  
+
+  
+  // If the user is not a premium member, show a message encouraging them to sign up
+       }
+        return <h2>Sign up for premium to unlock discounts!</h2>;
+       else {
+
+        }
+          return <h2>You get a 20% discount!</h2>;
+        if(isPremiumMember){
+  // If the user is a premium member, show the discount message
+  
+export function DiscountMessage({ isPremiumMember }) {
+// Export a function component named DiscountMessage that takes a prop 'isPremiumMember'
