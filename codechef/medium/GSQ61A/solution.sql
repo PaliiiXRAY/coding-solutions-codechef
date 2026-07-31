69e79cf111d9056b9c687d80c8ead3be2dc18bfc
Pallabi Pandi <pallabitheprincess74@gmail.com>
2026-07-31 16:04:10 +0530

    ✅ GSQ61A | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-07-31

--- codechef/medium/GSQ61A/solution.sql
+++ codechef/medium/GSQ61A/solution.sql
@@ -1,5 +1,6 @@
-/* Write a query to join the table 'student' and 'course' using 'Course_id' to match both the tables and output the joined table. */
-
-SELECT * from student
-join course
-on student.course_id = course.course_id;
+/* Write a query to join the tables 'student' and 'course' and output the same. Check if you can find the course with id ENG201 in the output */
+ SELECT * 
+ from student
+ 
+ join course
+ on student.Course_id = course.Course_id;
