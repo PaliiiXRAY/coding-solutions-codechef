a2c71cb9b9b900614b5e389278ca8c8690e06ea8
Pallabi Pandi <pallabitheprincess74@gmail.com>
2026-08-18 23:26:28 +0530

    ✅ AJUVG01 | Medium | SQL | Runtime: N/A | Memory: N/A | 2026-08-18

--- codechef/medium/AJUVG01/solution.sql
+++ codechef/medium/AJUVG01/solution.sql
@@ -1,7 +1,54 @@
--- your code goes here
-SELECT EmpID, Name,
-Department, Salary
+CREATE TABLE Customers (
+    CustomerID INTEGER PRIMARY KEY,
+    CustomerName TEXT
+);
 
-FROM Employees
-WHERE Salary > (SELECT AVG(SALARY) FROM Employees)
-ORDER BY Salary DESC;
+CREATE TABLE Orders (
+    OrderID INTEGER PRIMARY KEY,
+    CustomerID INTEGER,
+    OrderAmount INTEGER
+);
+
+CREATE TABLE Payments (
+    PaymentID INTEGER PRIMARY KEY,
+    OrderID INTEGER,
+    PaymentAmount INTEGER
+);
+
+INSERT INTO Customers VALUES
+(1,'Rahul'),
+(2,'Priya'),
+(3,'Amit'),
+(4,'Neha');
+
+INSERT INTO Orders VALUES
+(101,1,5000),
+(102,2,7000),
+(103,3,4000),
+(104,1,9000),
+(105,4,6000);
+
+INSERT INTO Payments VALUES
+(1,101,5000),
+(2,102,6500),
+(3,103,4000),
+(4,104,9500),
+(5,105,6000);
+
+SELECT c.CustomerID,
+     c.CustomerName, 
+     o.OrderID, 
+     o.OrderAmount, 
+     p.PaymentAmount
+FROM Customers AS c JOIN
+Orders AS o ON 
+c.CustomerID = o.CustomerID
+JOIN Payments AS p 
+ON p.OrderID = o.OrderID
+WHERE p.PaymentAmount >= o.OrderAmount
+AND EXISTS(
+ SELECT 1 FROM Orders o2
+ WHERE o2.CustomerID = c.CustomerID
+)
+ORDER BY  o.OrderAmount DESC;
+
